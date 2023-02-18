4e375d776195ad43010f76df35c875f91e4be7a0
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2023-02-19 01:31:30 +0530

    :hammer: add Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import styles from '@/styles/Footer.module.css'
+import { Space_Mono } from '@next/font/google'
+
+const spaceMono = Space_Mono({
+    weight: '400',
+    subsets: ['latin'],
+})
+
+export default function Footer({name}) {
+    const year = new Date().getFullYear()
+    return (
+        <div className={`${styles.footerWrapper} ${spaceMono.className}`}>
+            <p className={styles.footerText}>© {year} {name}</p>
+            <p className={styles.footerText}>Powered by <a href='/'>Devsite</a></p>
+        </div>
+    )
+}
