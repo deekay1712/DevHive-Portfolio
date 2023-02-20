c8bd50f90db7d027bddd7db4314295455fa35300
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2023-02-20 17:11:14 +0530

    :hammer: font fix

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,6 @@ export default function Home({userData}) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> 
-        <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet" />
       </Head>
       <Navbar userData={userData} />
       <LandingPage userData={userData} />
